427d7504d0b0be648f67d16dae01dfab60f9fcef
Unknown <Dremand@hotmail.com>
2018-05-13 10:37:13 -0400

    Made block off in scene. Working on a zoom script. Attached to camera but didn't turn on.

---
+++ MisJam2018GitHub/Assets/ZoomCam.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCam : MonoBehaviour
+{
+    public float zoomSpeed = 5f;
+
+    Camera cam;
+    float camFOV;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        camFOV = cam.fieldOfView;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            camFOV += zoomSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            camFOV -= zoomSpeed * Time.deltaTime;
+        }
+
+        cam.fieldOfView = Mathf.Clamp(camFOV, 20, 80);
+    }
+}
