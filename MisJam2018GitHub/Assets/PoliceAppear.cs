65c1a26b56ae6d3846b250fc8fe9af2a772c2c78
Unknown <shawngauvin@hotmail.com>
2018-05-13 12:26:43 -0400

    Police Appear scripts and music

---
+++ MisJam2018GitHub/Assets/PoliceAppear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceAppear : MonoBehaviour {
+    public GameObject UITool;
+    public GameObject PoliceOfficer;
+    public static bool FinalHardSlap;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(FinalHardSlap)
+        {
+            StartCoroutine(FinalSlapAnimation());
+        }
+	}
+
+    IEnumerator FinalSlapAnimation()
+    {
+        UITool.SetActive(true);
+        //instantiate police
+        Instantiate(PoliceOfficer, new Vector3(58.78f, 0.09f, 12.54f), Quaternion.identity);
+        Instantiate(PoliceOfficer, new Vector3(58.46f, 0.09f, 15.73f), Quaternion.identity);
+        Instantiate(PoliceOfficer, new Vector3(56.81f, 0.09f, 13.33f), Quaternion.identity);
+        Instantiate(PoliceOfficer, new Vector3(56.65f, 0.09f, 14.94f), Quaternion.identity);
+        yield return new WaitForSeconds(2);
+        UITool.SetActive(false);
+        
+    }
+}
