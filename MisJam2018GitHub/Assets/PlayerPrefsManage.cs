24b8b6e816165297b935b42ffcca905e70085a43
Unknown <Dremand@hotmail.com>
2018-05-13 15:10:34 -0400

    Sound into game
    Made a couple scripts. Changed Start scene to have options and exit button. Made start scene buttons work with animations

---
+++ MisJam2018GitHub/Assets/PlayerPrefsManage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManage : MonoBehaviour
+{
+
+    const string Master_Volume_Key = "master_volume";
+    const string SoundFX_Volume_Key = "SoundFX_volume";
+    const string Level_Key = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(Master_Volume_Key, volume);
+        }
+        else
+        {
+            Debug.Log("Master volume out of Range.");
+        }
+    }
+    public static void SetSoundFXVolume(float volume)
+    {
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(SoundFX_Volume_Key, volume);
+        }
+        else
+        {
+            Debug.Log("Master volume out of Range.");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(Master_Volume_Key);
+    }
+    public static float GetSoundFXVolume()
+    {
+        return PlayerPrefs.GetFloat(SoundFX_Volume_Key);
+    }
+}
---
+++ MisJam2018GitHub/Assets/optionsUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionsUp : MonoBehaviour {
+
+    public Animator startOptionsButtonsAnim;
+    public GameObject music;
+    public GameObject voice;
+
+    public void OptionsUp()
+    {
+        Invoke("OptionUpDelay", 1f);
+    }
+    void OptionUpDelay()
+    {
+        startOptionsButtonsAnim.SetTrigger("gotoOptions");
+        Invoke("MusicAndVoice", 2f);
+    }
+    void MusicAndVoice()
+    {
+        music.SetActive(true);
+        voice.SetActive(true);
+    }
+}
